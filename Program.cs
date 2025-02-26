49e1eacea9589688fdd6ec5e15ace613a502dd7c
ttetricas <estermeireles89@gmail.com>
2025-02-26 13:49:55 -0300

    Criação de programa que mostra minhas frutas preferidas!

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("--- Frutas que eu amo ---");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("1. Banana");
+Console.WriteLine("2. Maça");
+Console.WriteLine("3. Morango");
+Console.WriteLine("4. Manga");
+Console.WriteLine("5. Melancia");
